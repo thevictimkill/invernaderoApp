833dbd4d8d803958ba8434ed39c937c687ed74fd
thevictimkill <pedrof.ortega@alumnos.udg.mx>
2018-10-31 14:31:51 -0600

    actualizacion de servidor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 import {LoginPage} from "../pages/login/login";
+import {HttpModule} from "@angular/http";
 
 @NgModule({
   declarations: [
@@ -17,6 +18,7 @@ import {LoginPage} from "../pages/login/login";
   ],
   imports: [
     BrowserModule,
+    HttpModule,
     IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import {HomePage} from "../home/home";
+import {Http, HttpModule} from "@angular/http";
 
 /**
  * Generated class for the LoginPage page.
@@ -17,8 +18,10 @@ import {HomePage} from "../home/home";
 export class LoginPage {
 
   home = HomePage;
+  usuario = '';
+  password = '';
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams,public http: Http) {
   }
 
   ionViewDidLoad() {
@@ -26,7 +29,17 @@ export class LoginPage {
     console.log('ionViewDidLoad LoginPage');
   }
   clickHome(){
-    this.navCtrl.setRoot(this.home);
+    console.log(this.usuario);
+    console.log(this.password);
+
+    this.http.get('/home/')
+      .subscribe(data=> {
+        console.log(data.text())
+        }, error => {
+        console.log("error");
+    }
+      )
+    //this.navCtrl.setRoot(this.home);
   }
 
 }
